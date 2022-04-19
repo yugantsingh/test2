a0a8095775bb750d7195e4ac8d7a86a311910e0b
Yugant Singh <yugantx@gmail.com>
2022-04-19 16:30:36 +0530

    init

---
+++ build/precache-manifest.26327f42fc3c6cda8c7217e25a5c6301.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de35afe2e54cff191e594e7e565b5562",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d22ae3d15ebd5d1c22ef",
+    "url": "/static/css/2.9fca9f18.chunk.css"
+  },
+  {
+    "revision": "bdf45f2e32989fc95755",
+    "url": "/static/css/main.7301a5b8.chunk.css"
+  },
+  {
+    "revision": "d22ae3d15ebd5d1c22ef",
+    "url": "/static/js/2.a46ecc48.chunk.js"
+  },
+  {
+    "revision": "bdf45f2e32989fc95755",
+    "url": "/static/js/main.69306f00.chunk.js"
+  },
+  {
+    "revision": "51810371bf2cf35f1e22",
+    "url": "/static/js/runtime-main.85498786.js"
+  },
+  {
+    "revision": "937f6a18d4c4a533edf9b4c0bc512257",
+    "url": "/static/media/DatabaseFP.937f6a18.jpeg"
+  },
+  {
+    "revision": "ea3ba46bf14133ed92a0ecd8f99b5bc1",
+    "url": "/static/media/DoctorFP.ea3ba46b.jpeg"
+  },
+  {
+    "revision": "b5bfd2356c8b62923433ae1bbe684bfe",
+    "url": "/static/media/Meditech FP Final-01.b5bfd235.png"
+  },
+  {
+    "revision": "b8bd175f88316235c5431c1825c6ae13",
+    "url": "/static/media/Meditech FP Final-2-01.b8bd175f.png"
+  },
+  {
+    "revision": "0d2d38f141931247825d7ad1246ef4fd",
+    "url": "/static/media/Meditech.0d2d38f1.png"
+  },
+  {
+    "revision": "fcf4667076d07d20b3a30dd33f694656",
+    "url": "/static/media/jj.fcf46670.jpg"
+  }
+]);
